4309a42bd388bd9aa640941a27b701678f7934c5
Moin Hussain Moti <moinmoti@outlook.com>
2022-01-07 00:50:21 +0800

    Feat: New script based on plotly that indicates tolerance values like a heatmap.

--- plotMPT.py
+++ plotMPT.py
@@ -11,7 +11,7 @@ with open("MPT.csv") as f:
                     float(line.split(",")[3]),
                     float(line.split(",")[4]),
                     float(line.split(",")[5]),
-                    float(line.split(",")[6]),
+                    round(float(line.split(",")[6]), 2),
                 )
             )
         # if i == 10000: break
@@ -19,24 +19,53 @@ with open("MPT.csv") as f:
 fig = go.Figure()
 
 # Set axes properties
-fig.update_xaxes(range=[0, 7], showgrid=False)
-fig.update_yaxes(range=[0, 3.5])
+fig.update_xaxes(range=[-200, 200], showgrid=False, zeroline=False)
+fig.update_yaxes(range=[-100, 100], showgrid=False, zeroline=False)
 
 # Add shapes
-fig.add_shape(type="rect",
-    x0=1, y0=1, x1=2, y1=3,
-    line=dict(color="RoyalBlue"),
+colors = []
+for n in nodes:
+    # fig.add_shape(type="rect",
+    #     x0=n[0], y0=n[1], x1=n[2], y1=n[3],
+    #     fillcolor="green",
+    #     opacity=n[4],
+    #     text=str(n[4]),
+    # )
+    fig.add_trace(
+        go.Scatter(
+            mode="lines",
+            x=[n[0],n[0],n[2],n[2],n[0]],
+            y=[n[1],n[3],n[3],n[1],n[1]],
+            fill="toself",
+            name="",
+            text=str(n[4]),
+            fillcolor='rgba(0,0,0,'+str(n[4])+')',
+            line=dict(
+                color="black", width=1),
+        )
+    )
+
+fig.add_trace(
+    go.Scatter(
+        x=[None],
+        y=[None],
+        mode='markers',
+        marker=dict(
+            cmin=0,
+            cmax=1,
+            colorscale=[[0,'rgba(0,0,0,0)'],[1, 'rgba(0,0,0,1)']],
+            showscale=True,
+            colorbar=dict(
+                title="Node tolerance",
+                tickvals=[0, 0.25, 0.5, 0.75, 1],
+            )
+        )
+    )
 )
-fig.add_shape(type="rect",
-    x0=3, y0=1, x1=6, y1=2,
-    line=dict(
-        color="RoyalBlue",
-        width=2,
-    ),
-    fillcolor="LightSkyBlue",
-)
-fig.update_shapes(dict(xref='x', yref='y'))
-# fig.show()
+
+# fig.update_shapes(dict(xref='x', yref='y', line=dict(color="black")))
+fig.update_layout(showlegend=False)
+fig.show()
 
 
 # Create figure and axes
