7daa88f74d553192c6526a8315cb540995adce1a
Moin Hussain Moti <mhmoti@cse.ust.hk>
2022-03-04 16:01:41 +0800

    new automated script for running index.

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #cmake -G "Unix Makefiles" .
 cmake -S . -B build
 cmake --build build -j --clean-first
-cmake --install build
-QF="ST-q1e4-W"
-W=(1 10 50 90 99)
-for w in ${W[@]}; do
-    mkdir -p Experiments/$QF$w
-    ./Index $PWD $QF$w 128 128
+# cmake --install build
+Q=(Mix Rng Knn)
+T=(S T ST)
+W=(W E R)
+for q in ${Q[@]}; do
+    for t in ${T[@]}; do
+        for w in ${W[@]}; do
+            QF="${q}/${t}-L1e7-I1e6-${w}H"
+            echo "Experiments/${QF}"
+            mkdir -p "Experiments/${QF}"
+            ./Index $PWD $QF 128 128
+            # lldb -- Index $PWD $QF 128 128
+        done
+    done
 done
 # lldb -- Index $PWD $QF $1 $2
-# params=(128 256 512 1024 2048)
-# for p in "${params[@]}"; do
-#     ./Index $PWD $QF "$p" "$p"
-# done
-#python visualizeHopTree.py
